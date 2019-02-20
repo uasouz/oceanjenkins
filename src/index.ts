a912faece2ba5f476aa9360684003b997a8332fd
hectorcanaimero <rodriguez.hectoralejandro@gmail.com>
2019-02-19 20:44:19 -0400

    Corrigindo nome da variavel

--- src/index.ts
+++ src/index.ts
@@ -11,5 +11,5 @@ const restRouter = new RestRouter(Service.expressApp);
 //start our server
 Service.server.listen(process.env.PORT || 8999, () => {
     // @ts-ignore
-    console.log(`Welcome back,Commander. I am a service running at port ${Abaddon.server.address().port}`);
+    console.log(`Welcome back,Commander. I am a service running at port ${Service.server.address().port}`);
 });
